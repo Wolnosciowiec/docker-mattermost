6a49a255d58c57cf700e8fdd41fd5fd4f3fc1728
Andrew Johnson <wesoly.krzysztofa@gmail.com>
2017-09-11 17:15:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ case ${1} in
     initialize
     configure
     migrate
-    ./bin/platform -config ${MATTERMOST_CONF_DIR}/config.json
+    /usr/bin/mattermost-platform -config ${MATTERMOST_CONF_DIR}/config.json
     ;;
 
   app:migrate)
